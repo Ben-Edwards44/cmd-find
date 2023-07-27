a4f5d82f55aa9db60aa4bca6ab9548de842331f4
Ben-Edwards44 <benjedwards007@icloud.com>
2023-07-27 18:13:21 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -18,10 +18,8 @@ setup(
     url = "https://github.com/Ben-Edwards44/cmd-find",
     py_modules = ["main", "app"],
     packages = find_packages(),
-    install_requires = [open_file("requirements.txt").split("\n")],
+    install_requires = ["numpy>=1.24.1",
+                        "sentence_transformers>=2.2.2"],
     python_requires=">=3.8",
-    entry_points = """
-        [console_scripts]
-        cmd-find=main:main
-    """
+    entry_points = {"console_scripts" : ["cmd-find = main:main"]}
 )
