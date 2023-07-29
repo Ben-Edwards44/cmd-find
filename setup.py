6a5fa4d7dce0b6aa6f9ef7fd4a3a4c3567082b67
Ben-Edwards44 <benjedwards007@icloud.com>
2023-07-29 18:22:35 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,12 +15,18 @@ setup(
     description = "A CLI tool that finds Linux commands from their descriptions",
     long_description = open_file("README.md"),
     long_description_content_type = "text/markdown",
-    url = "https://github.com/Ben-Edwards44/cmd_find",
+    url = "https://github.com/Ben-Edwards44/cmd-find",
     py_modules = ["main", "app"],
     packages = find_packages(exclude=["create_file"]),
     install_requires = ["numpy",
                         "sentence_transformers"],
     python_requires=">=3.8",
-    entry_points = {"console_scripts" : ["cmd_find = main:main", "cmd-find = main:main"]},
-    license="MIT"
+    entry_points = {"console_scripts" : ["cmd_find = main:main", 
+                                         "cmd-find = main:main"]},
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "Environment :: Console"
+    ]
 )
